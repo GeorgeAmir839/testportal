91fd48b0e1d64383fade9d73aba6fb0301e84996
George amir <georgeamir839@gmail.com>
2022-08-05 17:32:13 +0200

    add dashboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->nullable();
+            $table->string('user_type')->default('employee');
+            $table->unsignedBigInteger('company_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
